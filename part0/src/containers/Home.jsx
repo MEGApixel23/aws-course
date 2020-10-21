d55ae68e7c06447db62e2f7ae9364781b3b9228a
Igor Omelchenko <megapixel23@gmail.com>
2020-10-22 00:18:33 +0300

    Lessons page added.

--- part0/src/containers/Home.jsx
+++ part0/src/containers/Home.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import logo from '../logo.svg';
 
-const Home = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Home page (updated 3)
-      </p>
-    </header>
-  </div>
-);
+const Home = () => {
+  const [lessons, setLessons] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.megapixel23.com/lessons')
+      .then(async (response) => {
+        const data = await response.json();
+
+        setLessons(data);
+      })
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          {lessons.map(({ id, url, title }) => (
+            <div key={id}>
+              <a href={url}>{title}</a>
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default Home;

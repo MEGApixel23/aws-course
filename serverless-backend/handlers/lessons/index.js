8df1f30e896854a1015f0ec676f8d1a3992eba5b
Igor Omelchenko <megapixel23@gmail.com>
2020-12-06 19:57:00 +0200

    DynamoDB lessons table added.

--- serverless-backend/handlers/lessons/index.js
+++ serverless-backend/handlers/lessons/index.js
@@ -1,7 +1,15 @@
+const { DynamoDB } = require('aws-sdk');
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
 module.exports.main = async () => {
+  const result = await dynamoDb.scan({
+    TableName: process.env.LESSONS_TABLE,
+  }).promise();
+
   return {
     statusCode: 200,
-    body: JSON.stringify([]),
+    body: JSON.stringify(result.Items),
     headers: {
       'Access-Control-Allow-Headers' : 'Content-Type',
       'Access-Control-Allow-Origin': '*',
